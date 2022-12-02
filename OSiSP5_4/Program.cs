25e6e2d941c540018b218d82406aff308d91acd4
Екатерина <katya.borodich@list.ru>
2022-12-02 13:44:36 +0300

    last

--- OSiSP5_4/Program.cs
+++ OSiSP5_4/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Threading;
 
 namespace OSiSP5_4
 {
@@ -12,16 +13,22 @@ namespace OSiSP5_4
         const String SRC_FILEPATH = "..\\..\\..\\..\\src.txt";
         const String OUT_FILEPATH = "..\\..\\..\\..\\out.txt";
         const int THREAD_COUNT = 6;
+        static List<List<String>> allWords;
 
         static void Main(string[] args)
         {
+            allWords = new List<List<String>>();
             var words = readFile();
-            var tasks = initTasks(words);
+            var tasks = initTasks(ref words);
             Queue queueTasks = new Queue();
             for (int i = 0; i < THREAD_COUNT; i++)
                 queueTasks.Enqueue(tasks[i]);
-
-            Console.ReadKey();
+            ExecuteTasks(queueTasks);
+            words.Clear();
+            words = allWords[0];
+            for (int i = 1; i < allWords.Count; i++)
+                words = SortLast(words, allWords[i]);
+            writeFile(words);
         }
 
         static List<String> readFile()
@@ -47,7 +54,7 @@ namespace OSiSP5_4
             try
             {
                 StreamWriter sw = new StreamWriter(OUT_FILEPATH);
-                sw.Write(result);
+                sw.Write(String.Join('\n', result));
                 sw.Close();
             } 
             catch(Exception e)
@@ -56,7 +63,25 @@ namespace OSiSP5_4
             }
         }
 
-        static List<Task> initTasks(List<String> words)
+        static void ExecuteTasks(Queue queueTasks)
+        {
+            Thread[] threads = new Thread[THREAD_COUNT];
+            for (int i = 0; i < THREAD_COUNT; i++)
+            {
+                threads[i] = new Thread(() => {
+                    if (queueTasks.Count != 0)
+                    {
+                        Task task = (Task)queueTasks.Dequeue();
+                        task.Start();
+                    }
+                });
+                threads[i].Start();
+            }
+            for (int i = 0; i < THREAD_COUNT; i++)
+                threads[i].Join();
+        }
+
+        static List<Task> initTasks(ref List<String> words)
         {
             List<Task> tasks = new List<Task>();
             int wordsPerThread = (int)Math.Ceiling((float)words.Count() / (float)THREAD_COUNT);
@@ -68,13 +93,24 @@ namespace OSiSP5_4
                 {
                     part.Add(words[j]);
                 }
+                allWords.Add(part);
                 Task task = new Task(() =>
                 {
-                    part.Sort();
+                    lock(allWords)
+                        allWords[i].Sort();
                 });
                 tasks.Add(task);
             }
             return tasks;
+        }
+
+        static List<String> SortLast(List<String> list1, List<String> list2)
+        {
+            List<String> result = new List<String>(list1);
+            for(int i = 0; i < list2.Count; i++)
+                result.Add(list2[i]);
+            result.Sort();
+            return result;
         }
     }
 }
